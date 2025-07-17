191260a29121c9240238fa131791d407a88ba047
Artem Isachenkov <artemyt237@gmail.com>
2025-07-17 17:40:59 +0300

    add psql storage

--- UsersManager/go.mod
+++ UsersManager/go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/joho/godotenv v1.5.1
+	github.com/pressly/goose v2.7.0+incompatible
+	github.com/pressly/goose/v3 v3.24.3
 	google.golang.org/grpc v1.74.0
 )
 
@@ -14,7 +16,12 @@ require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250528174236-200df99c418a // indirect
---
+++ UsersManager/internal/storage/users/psql/users.go
@@ -0,0 +1,69 @@
+package psql
+
+import (
+	"context"
+	"database/sql"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"usersmanager/internal/domain/models"
+
+	"github.com/google/uuid"
+	"github.com/pressly/goose/v3"
+)
+
+type UsersPsqlStorage struct {
+	log       *slog.Logger
+	DB        *sql.DB
+	TableName string
+}
+
+func New(log *slog.Logger, connStr string, tableName string) *UsersPsqlStorage {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+
+	wd, _ := os.Getwd()
+	migrationPath := filepath.Join(wd, "app", "migrations")
+	if err := goose.Up(db, migrationPath); err != nil {
+		panic(err)
+	}
+
+	return &UsersPsqlStorage{
+		log:       log,
+		DB:        db,
+		TableName: tableName,
+	}
+}
+
+func (u *UsersPsqlStorage) Close() {
+	if err := u.DB.Close(); err != nil {
+		panic(err)
+	}
+}
+
+// GetUsers implements app.IUsersStorage.
+func (u *UsersPsqlStorage) GetUsers(ctx context.Context) ([]models.User, error) {
+	panic("unimplemented")
+}
+
+// GetUserById implements app.IUsersStorage.
+func (u *UsersPsqlStorage) GetUserById(ctx context.Context, uid uuid.UUID) (models.User, error) {
+	panic("unimplemented")
+}
+
+// Insert implements app.IUsersStorage.
+func (u *UsersPsqlStorage) Insert(ctx context.Context, user models.User) (models.User, error) {
+	panic("unimplemented")
+}
+
+// Update implements app.IUsersStorage.
+func (u *UsersPsqlStorage) Update(ctx context.Context, uid uuid.UUID, user models.User) (models.User, error) {
+	panic("unimplemented")
+}
+
+// Delete implements app.IUsersStorage.
+func (u *UsersPsqlStorage) Delete(ctx context.Context, uid uuid.UUID) (models.User, error) {
+	panic("unimplemented")
+}
